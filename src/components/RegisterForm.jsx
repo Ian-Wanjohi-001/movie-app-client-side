5f1719428eff559c24932310baf81a74a8a7e3bb
jacksonmwangi895@gmail.com <jacksonmwangi1895@gmail.com>
2023-07-01 09:22:59 +0300

    div rectifications

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,15 +1,12 @@
-import './registerform.css'
+import './registerform.css';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 import { RiUserFill, RiMailFill, RiLockPasswordFill } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import {toast, ToastContainer} from 'react-toastify'
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-
-
 
 const RegisterForm = () => {
   const validationSchema = Yup.object().shape({
@@ -33,50 +30,56 @@ const RegisterForm = () => {
   });
 
   const onSubmit = (data) => {
-    console.log(data); 
-    axios.post("https://moviereccomendationapi.azurewebsites.net/auth/register", data)
-    .then((response) => {
-      response.data.message && toast.success(response.data.message)
-      console.log(response)
-    })
-    .catch ((error) => {
-      toast.error(error.response.data.error);
-    })
+    console.log(data);
+    axios
+      .post('https://moviereccomendationapi.azurewebsites.net/auth/register', data)
+      .then((response) => {
+        response.data.message && toast.success(response.data.message);
+        console.log(response);
+      })
+      .catch((error) => {
+        toast.error(error.response.data.error);
+      });
   };
 
-  return (<>
-    <div className="register-form">
-      <h2>Register</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-field">
-          <RiUserFill />
-          <input type="text" {...register('username')} placeholder="Username" />
-          {errors.username && <p className="error-message">{errors.username.message}</p>}
-        </div>
-        <div className="form-field">
-          <RiMailFill />
-          <input type="text" {...register('email')} placeholder="Email" />
-          {errors.email && <p className="error-message">{errors.email.message}</p>}
-        </div>
-        <div className="form-field">
-          <RiLockPasswordFill />
-          <input type="password" {...register('password')} placeholder="Password" />
-          {errors.password && <p className="error-message">{errors.password.message}</p>}
-        </div>
-        <div className="form-field">
-          <RiLockPasswordFill />
-          <input type="password" {...register('confirmPassword')} placeholder="Confirm Password" />
-          {errors.confirmPassword && (
-            <p className="error-message">{errors.confirmPassword.message}</p>
-          )}
-        </div>
-        <button type="submit">Register</button>
-      </form>
-      <p className="redirect-message">
-        Already have an account? <Link to="/login">Login</Link>
-      </p>
-    </div>
-    <ToastContainer />
+  return (
+    <>
+      <div className="register-form">
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-field">
+            <RiUserFill />
+            <input type="text" {...register('username')} placeholder="Username" />
+            {errors.username && <p className="error-message">{errors.username.message}</p>}
+          </div>
+          <div className="form-field">
+            <RiMailFill />
+            <input type="text" {...register('email')} placeholder="Email" />
+            {errors.email && <p className="error-message">{errors.email.message}</p>}
+          </div>
+          <div className="form-field">
+            <RiLockPasswordFill />
+            <input type="password" {...register('password')} placeholder="Password" />
+            {errors.password && <p className="error-message">{errors.password.message}</p>}
+          </div>
+          <div className="form-field">
+            <RiLockPasswordFill />
+            <input
+              type="password"
+              {...register('confirmPassword')}
+              placeholder="Confirm Password"
+            />
+            {errors.confirmPassword && (
+              <p className="error-message">{errors.confirmPassword.message}</p>
+            )}
+          </div>
+          <button type="submit">Register</button>
+        </form>
+        <p className="redirect-message">
+          Already have an account? <Link to="/login">Login</Link>
+        </p>
+      </div>
+      <ToastContainer />
     </>
   );
 };
